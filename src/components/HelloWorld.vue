21465f1e41cbe5b79ab4f973b846737a0990cd36
Bestian Tang 小巴 <bestian@gmail.com>
2021-01-26 20:49:08 +0800

    icon

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,73 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="hello" :class="{ black: win }">
+    <div class="hole" v-for="(k, idx) in holes" :key="idx">
+      <div class="mole" :class="{ hide: k }">
+        <a @click = "hit(idx)" :class="{ hit: hiting }">
+          <img src="/mole.png">
+        </a>
+      </div>
+    </div>
+    <h2>目前{{score}}分</h2>
+    <h2>剩下{{t}}秒</h2>
+    <button v-show="end" @click="end = false; t = 60; score = 0; win = false">再來一局</button>
+    <a id = "win" v-show="win" @click="end = false; t = 60; score = 0; win = false">
+      <img src="/win2.png"/>
+    </a>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
+  methods: {
+    unhit() {
+      this.hiting = false;
+    },
+    hit(k) {
+      this.hiting = true;
+      if (!this.end) {
+        this.holes[k] = true;
+        this.$forceUpdate();
+        this.score += 1;
+      }
+      setTimeout(this.unhit, 50);
+    },
+    time() {
+      if (!this.end) {
+        this.t -= 1;
+      }
+      if (this.t <= 0) {
+        this.end = true;
+        if (this.score >= 30) {
+          this.win = true;
+        }
+      }
+    },
+    reset() {
+      if (!this.end) {
+        this.holes = this.holes.map(() => [true, false][Math.floor(Math.random() * 2)]);
+      }
+    },
+  },
+  mounted() {
+    this.reset();
+    setInterval(this.reset, 3000);
+    setInterval(this.time, 1000);
+  },
+  data() {
+    return {
+      hiting: false,
+      win: false,
+      end: false,
+      score: 0,
+      t: 60,
+      holes: [
+        false, false, false,
+        false, false, false,
+        false, false, false,
+      ],
+    };
+  },
   props: {
     msg: String,
   },
@@ -43,18 +76,50 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+
+.black {
+  background-color: black;
+  background-image: url('/color.png');
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+
 a {
-  color: #42b983;
+  cursor: url('/hammer2.png'), pointer;
 }
+
+a.hit {
+  cursor: url('/hammer3.png'), pointer;
+}
+
+.hole {
+  display: inline-block;
+  overflow: hidden;
+}
+
+.mole {
+  width: 28vw;
+}
+
+.mole img {
+  width: 100%;
+  position: relative;
+  top: 0;
+  transition: all 2s ease;
+}
+
+.mole.hide img {
+  position: relative;
+  top: 300px;
+}
+
+#win {
+  position: fixed;
+  width: 50vw;
+  top: 10vh;
+  left: 25vw;
+}
+
+#win img {
+  width: 100%;
+}
+
 </style>
