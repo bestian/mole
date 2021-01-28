60190a54349c9479d4f5548c870516d5d22e6077
Bestian Tang <bestian@gmail.com>
2021-01-28 21:53:59 +0800

    deploy

---
+++ precache-manifest.6d978ea43e6aa3b0108458a39526eba5.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "157e461b5d24739096f96ad80c33299b",
+    "url": "/mole/color.png"
+  },
+  {
+    "revision": "9b6e87c6379a1d7341a0",
+    "url": "/mole/css/app.da772e1e.css"
+  },
+  {
+    "revision": "e5cd32e23270cef9efcede0b331445e3",
+    "url": "/mole/hammer.cur"
+  },
+  {
+    "revision": "bef28b170fe101f18bc0cb02fd07d6aa",
+    "url": "/mole/hammer.jpg"
+  },
+  {
+    "revision": "8671e64daad236f8caac7a69c8b5c596",
+    "url": "/mole/hammer2.png"
+  },
+  {
+    "revision": "2a587ee3db298705a517707a06c3bef3",
+    "url": "/mole/hammer3.png"
+  },
+  {
+    "revision": "157e461b5d24739096f96ad80c33299b",
+    "url": "/mole/img/color.157e461b.png"
+  },
+  {
+    "revision": "8671e64daad236f8caac7a69c8b5c596",
+    "url": "/mole/img/hammer2.8671e64d.png"
+  },
+  {
+    "revision": "2a587ee3db298705a517707a06c3bef3",
+    "url": "/mole/img/hammer3.2a587ee3.png"
+  },
+  {
+    "revision": "bee4bb894ce66a28f95ba4a19641d3e1",
+    "url": "/mole/img/mole.bee4bb89.png"
+  },
+  {
+    "revision": "3df1a5d6e83ea15c116311576d0a29cb",
+    "url": "/mole/img/win2.3df1a5d6.png"
+  },
+  {
+    "revision": "5a36ee0f322377a754221f379099bbfd",
+    "url": "/mole/index.html"
+  },
+  {
+    "revision": "bcfe0729fb7c5f167a39",
+    "url": "/mole/js/about.b9b7d886.js"
+  },
+  {
+    "revision": "9b6e87c6379a1d7341a0",
+    "url": "/mole/js/app.31e1a257.js"
+  },
+  {
+    "revision": "5f67e4d3e7f8b987adf8",
+    "url": "/mole/js/chunk-vendors.c927884d.js"
+  },
+  {
+    "revision": "06e48e8892cb446d467aa46996c4138e",
+    "url": "/mole/manifest.json"
+  },
+  {
+    "revision": "1731f478322e7324905e7b64bdbbda4d",
+    "url": "/mole/mole.jpg"
+  },
+  {
+    "revision": "bee4bb894ce66a28f95ba4a19641d3e1",
+    "url": "/mole/mole.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/mole/robots.txt"
+  },
+  {
+    "revision": "2579075ce23e14975521580c2c1fd5ef",
+    "url": "/mole/win.jpg"
+  },
+  {
+    "revision": "d1ba0ee5179635ffe8b62efe7a7e4c7b",
+    "url": "/mole/win.png"
+  },
+  {
+    "revision": "3df1a5d6e83ea15c116311576d0a29cb",
+    "url": "/mole/win2.png"
+  }
+]);
